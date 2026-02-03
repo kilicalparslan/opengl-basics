48c3a5b83955f5bcaf6227f9ce7a55e0b0062d06
kilicalparslan <kilicalparslan1998@gmail.com>
2026-02-03 23:32:41 +0300

    window created with bg color

--- Main.cpp
+++ Main.cpp
@@ -4,26 +4,54 @@
 
 int main() 
 {
+
+	//Initialize GLFW
 	glfwInit();
+
+	// Tell GLFW what version of OpenGL (4.6)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+	// Tell GLFW using the CORE profile
+	// That means only the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OPENGL Tutorial", NULL, NULL);
+	// Create a GLFWwindow object of 800 by 800 pixels, name it
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL Basics", NULL, NULL);
 
+	// Error check if the window fails to create
 	if (window == NULL) {
 		std::cout << "Failed to create window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+	// Introduce the window  into the current context
 	glfwMakeContextCurrent(window);
 
+	// Load GLAD to configure OpenGL
+	gladLoadGL();
+
+	// Specify the viewport of OpenGL in the window
+	// x = 0 to 800, y = 0 to 800
+	glViewport(0, 0, 800, 800);
+
+	// Specify the color of the background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
+	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
