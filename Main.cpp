93a2676a1ad07ddcb2eb102dfc810db3b10b8e00
Alp <kilicalparslan1998@gmail.com>
2026-02-12 11:31:05 +0300

    shape translated and rotated

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,10 @@
 #include <stb_image.h>
 #include "shader_s.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -154,12 +158,20 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
+
+        // transformations
+        glm::mat4 transform = glm::mat4(1.0f); 
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
         
         ourShader.setFloat("mixValue", mixValue);
 
+        ourShader.use();
+        // get matrix's uniform location and set matrix
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
         // render container
-        ourShader.use();
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
