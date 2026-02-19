36bcd70d0a03c711e6ca3a850a38e9cc80e3d523
Alp <kilicalparslan1998@gmail.com>
2026-02-19 14:54:17 +0300

    convert shaders to view-space lighting

--- assets/shaders/2_colors/colorsVS.glsl
+++ assets/shaders/2_colors/colorsVS.glsl
@@ -4,6 +4,9 @@ layout (location = 1) in vec3 aNormal;
 
 out vec3 FragPos;
 out vec3 Normal;
+out vec3 LightPos;
+
+uniform vec3 lightPos;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,8 +14,8 @@ uniform mat4 projection;
 
 void main()
 {
-	FragPos = vec3(model * vec4(aPos, 1.0));
-	Normal = aNormal;
-
-	gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    FragPos = vec3(view * model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(view * model))) * aNormal;
+    LightPos = vec3(view * vec4(lightPos, 1.0));
 }
